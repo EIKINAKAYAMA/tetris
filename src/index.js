6b632270611ba3d90eea7aea61213bea1798b4eb
EIKI NAKAYAMA <eiki111ixa@icloud.com>
2021-05-20 20:45:14 +0900

    check-code
    check-code

--- src/index.js
+++ src/index.js
@@ -5,59 +5,59 @@ import './index.css';
 const x = 8; //左右隠された一列を含むものとする。
 const y = 10;
 const maxNum = x*y
-const midleX = Math.floor(x/2);
+const middleX = Math.floor(x/2);
 
-
-//ブロックの種類、テンプレート
-function blocktemplate(midleX, x){
+//テトリミノの設定用関数
+function blocktemplate(middleX, x){
   const blocks = 
   [
     {
-      figures: [midleX-2,midleX-1, midleX, midleX+1],
+      figures: [middleX-2,middleX-1, middleX, middleX+1],
       color: "lightblue",
       angle: 0,
     },
     {
-      figures: [midleX,midleX+1, midleX+x, midleX+1+x],
+      figures: [middleX-1,middleX, middleX+x-1, middleX+x],
       color: "yellow",
       angle: 0,
     },
     {
-      figures: [midleX, midleX+1, midleX-1+x, midleX+x],
+      figures: [middleX, middleX+1, middleX-1+x, middleX+x],
       color: "green",
       angle: 0,
     },
     {
-      figures: [midleX-1, midleX, midleX+x, midleX+x+1],
+      figures: [middleX-1, middleX, middleX+x, middleX+x+1],
       color: "red",
       angle: 0,
     },
     {
-      figures: [midleX-1, midleX+x-1, midleX+x, midleX+x+1],
+      figures: [middleX-1, middleX+x-1, middleX+x, middleX+x+1],
       color: "blue",
       angle: 0,
     },
     {
-      figures: [midleX+1, midleX+x-1, midleX+x, midleX+x+1],
+      figures: [middleX+1, middleX+x-1, middleX+x, middleX+x+1],
       color: "orange",
       angle: 0,
     },
     {
-      figures: [midleX, midleX+x-1, midleX+x, midleX+x+1],
+      figures: [middleX, middleX+x-1, middleX+x, middleX+x+1],
       color: "purple",
       angle: 0,
     },
   ]
   return blocks
+  // const blocks = blocks_without_angle.map(f => {f.angle = 0 return f}) 
+  // const blocks = blocks_without_angle.map(f =>  ({...f , angle : 0})) 
 }
-
 
 class TetrisInformationPanel extends React.Component {
   render() {
     return (
       <div>
-      <p>Score:{this.props.score}</p>
-      <p>{this.props.message}</p>
+        <p>Score:{this.props.score}</p>
+        <p>{this.props.message}</p>
       </div>
     );
   }
@@ -68,28 +68,32 @@ function Square(props) {
     <td className = {`block-type-${props.name}`}></td>
   )
 }
-  
+
+//次のブロックを表示する為のtableクラス
 class Table extends React.Component {
+
   renderSquare(i){
     const table = Array(16).fill(null)
-    return <Square name = {table[i]} />
+    return <Square key={`table${i}`} name = {table[i]} />
   }
 
   render() {
-    const tbody = [];
-    const templateblocks = blocktemplate(2, 4)
+    const next_x = 2
+    const next_y = 4
+    const templateblocks = blocktemplate(next_x, next_y)
     const blocks = this.props.color? templateblocks.filter(f => f.color === this.props.color) : [{figures: [-1]}]
     const figures = blocks[0].figures
-    for(let i=0; i< 2; i++){
-      const row = [];
-      for (let j = 0; j < 4; j++){
-        var num = i * 4 + j
-        figures.includes(num)
-        ? row.push(<td className = {`block-type-${this.props.color}`}></td>)
-        : row.push(this.renderSquare(num));
-      }
-      tbody.push(<tr className = {`table-row`}>{row}</tr>)
-    }
+
+    const tbody = [...Array(next_x)].map((_, i) => {
+      const row = [...Array(next_y)].map((_, j) =>　{
+        const num = i * 4 + j
+        return figures.includes(num)
+        ? <td key={`table-td ${num}`} className = {`block-type-${this.props.color}`}></td>
+        : this.renderSquare(num);
+      })
+      
+      return <tr  key={`table-tr ${i}`} className = {`table-row`}>{row}</tr>
+    })
 
     return (
       <table className="board-table">
@@ -103,30 +107,21 @@ class Table extends React.Component {
 
 //盤面
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    }
-  }
-  
-  renderSquare(i){
-    return <Square name = {this.props.board[i]} />
-  }
-    
-  //ここでBordのNumにも色をつける。
-  render() {
-    const tbody = [];
 
-    for(let i=0; i< y; i++){
-      const row = [];
-      for (let j = 0; j < x; j++){
-        let num = i * x + j
-        this.props.figures.includes(num)
-        ? row.push(<td className = {`block-type-${this.props.color}`}></td>)
-        : row.push(this.renderSquare(num));
-      }
-      tbody.push(<tr className = {`table-row`}>{row}</tr>)
-    }
+  renderSquare(i){
+    return <Square key={`board${i}`} name = {this.props.board[i]} />
+  }
+
+  render() {
+    const tbody = [...Array(y)].map((_, i) => {
+      const row = [...Array(x)].map((_, j) => {
+        const num = i * x + j
+        return this.props.figures.includes(num)
+        ? <td key={`board-td${num}`} className = {`block-type-${this.props.color}`}></td>
+        : this.renderSquare(num)
+      })
+      return <tr key={`board-tr${i}`} className = {`table-row`}>{row}</tr>
+    })
 
     return (
       <table className="board-table">
@@ -145,19 +140,15 @@ class Tetris extends React.Component {
     this.state = {
       currentBlock: { //今のブロック
         figures: [],  //スタート座標
-        color: null,   //色
+        color: "next",   //色
         angle: null,
       },
-      nextBlock: {    //次落ちる予定のブロック
-        figures: [],
-        color: null,
-        angle: null,
-      },
+      nextBlock: this.cleateblock(),
       histories: [{　　//履歴
         board: Array(maxNum).fill(null)
       }],
       score: 0,
-      message: ""
+      message: "",
     }
     //キー操作が行われた時
     window.onkeydown = (e) => {
@@ -200,7 +191,7 @@ class Tetris extends React.Component {
         rotated_figures[2] = figures[2]
         rotated_figures[3] = figures[3]-x+1 
       }else{
-        alert("NG")
+        alert("想定外の挙動です。ご連絡いただけますと幸いです。")
       }
     }else if(color === "green"){
       if(angle=== 0 || angle===180){
@@ -214,7 +205,7 @@ class Tetris extends React.Component {
         rotated_figures[2] = figures[2]+x
         rotated_figures[3] = figures[3]+1
       }else{
-        alert("NG")
+        alert("想定外の挙動です。ご連絡いただけますと幸いです。")
       }
       
     }else if(color === "red"){
@@ -229,7 +220,7 @@ class Tetris extends React.Component {
         rotated_figures[2] = figures[2]
         rotated_figures[3] = figures[3]-x+1
       }else{
-        alert("NG")
+        alert("想定外の挙動です。ご連絡いただけますと幸いです。")
       }
 
     }else if(color === "blue"){
@@ -254,7 +245,7 @@ class Tetris extends React.Component {
         rotated_figures[2] = figures[2]
         rotated_figures[3] = figures[3]+x+1
       }else{
-        alert("NG")
+        alert("想定外の挙動です。ご連絡いただけますと幸いです。")
       }
       
     }else if(color === "orange"){
@@ -279,7 +270,7 @@ class Tetris extends React.Component {
         rotated_figures[2] = figures[2]
         rotated_figures[3] = figures[3]+x+1
       }else{
-        alert("NG")
+        alert("想定外の挙動です。ご連絡いただけますと幸いです。")
       }
       
     }else if(color === "purple"){
@@ -304,12 +295,12 @@ class Tetris extends React.Component {
         rotated_figures[2] = figures[2]
         rotated_figures[3] = figures[3]+x+1
       }else{
-        alert("NG")
+        alert("想定外の挙動です。ご連絡いただけますと幸いです。")
       }
     }else{
       return;
     }
-
+    
     if( rotated_figures.find(f => f % x === 0 || f % x === x - 1)){
       return;
     }else if(rotated_figures.find(f => board[f - 1] !== null)){
@@ -342,7 +333,7 @@ class Tetris extends React.Component {
       //通常時は左にズラすのみ。
       this.setState({
         currentBlock: {
-          figures : figures.map(m => m = m - 1),
+          figures : figures.map(m => m -= 1),
           color : this.state.currentBlock.color,
           angle : this.state.currentBlock.angle, 
         }
@@ -367,7 +358,7 @@ class Tetris extends React.Component {
       //通常時は左にズラすのみ。
       this.setState({
         currentBlock: {
-          figures : figures.map(m => m = m + 1),
+          figures : figures.map(m => m += 1),
           color : this.state.currentBlock.color,
           angle : this.state.currentBlock.angle,
         }
@@ -381,13 +372,17 @@ class Tetris extends React.Component {
     const current = history[history.length - 1];
     const board = current.board;
     const figures = this.state.currentBlock.figures.slice(0);
+
     //任意のブロック配列に対して、ブロックにおける最下層のFix判定用配列を作成。
     const filteredBottomFigure = figures.filter(f => figures.find(figure => figure === f + x))
     const JudgeBottomFigure = figures.filter(f => filteredBottomFigure.indexOf(f) === -1)
+
     // 配列の値のいずれかが、最下層、又は下に色がある時、Fix
     if( JudgeBottomFigure.find(f => maxNum < f + x) || JudgeBottomFigure.find(f => board[f + x] !== null)){
+      const minY = Math.floor(figures.reduce((a,b) => Math.min(a,b))/x)
+      const maxY = Math.floor(figures.reduce((a,b) => Math.max(a,b))/x)
       this.fixblock();
-      this.deleteLine();
+      this.deleteLine(minY, maxY);
       this.setState({
         histories: this.state.histories,
         currentBlock: {
@@ -400,7 +395,7 @@ class Tetris extends React.Component {
       this.setState({
         currentBlock: {
           figures : figures.map((figure) => {
-            return figure = figure + x
+            return figure += x
           }),
           color : this.state.currentBlock.color,
           angle : this.state.currentBlock.angle,
@@ -413,10 +408,8 @@ class Tetris extends React.Component {
     const history = this.state.histories;
     const current = history[history.length - 1];
     const board = current.board
-    const figures = this.state.currentBlock.figures
-    const color = this.state.currentBlock.color
 
-    figures.forEach(f => board[f] = color)
+    this.state.currentBlock.figures.forEach(f => board[f] = this.state.currentBlock.color)
 
     this.setState({
       histories: history.concat([{
@@ -428,43 +421,39 @@ class Tetris extends React.Component {
   //空白行を削除して点数加算
   delete(){
     const history = this.state.histories;
-    const current = history[history.length - 1];
-    const board = current.board
-    const copyboard = board.slice(0)
-    const score = this.state.score;
+    const copyboard = history[history.length - 1].board.slice(0)
     let LineConter = 0;
 
-    if(board.includes("delete")){
-      for(var k=0; k < y; k++){
-        if(copyboard[x*k] === "delete"){
-          LineConter++;
-          copyboard.splice(x*k, x)
-          for(var j=0; j < x; j++){
-            copyboard.splice(1, 0, null)
-          }
-        }
+    [...Array(y)].map((_,i) => {
+      if(copyboard[x*i] === "delete"){
+        return (
+          copyboard.splice(x*i, x),
+          LineConter++,
+          [...Array(x)].map(()=> copyboard.splice(1, 0, null))
+        )
+      }else{
+        return null
       }
-      this.setState({
-        histories: history.concat([{
-          board: copyboard
-        }]),
-        score: score + LineConter*100*(LineConter*1)
-      })
-    }else{
-      return
-    }
+    })
+
+    this.setState({
+      histories: history.concat([{
+        board: copyboard
+      }]),
+      score: this.state.score + LineConter*100*(LineConter*1)
+    })
   }
 
   // この関数は着色ラインを消すことのみを目的とする。
-  deleteLine(){
+  deleteLine(minY, maxY){
     const history = this.state.histories;
     const current = history[history.length - 1];
     const copyhistories = this.state.histories.slice(0, history.length -1)
     const board = current.board
     const copyboard = board.slice(0)
-    
+
     //削除されるべきラインを全て”Null”にする
-    for(var i=0; i < y; i++){
+    for(var i = minY; i <= maxY; i++){
       const Line = copyboard.slice(1+x*i, x*(i+1)-1);
       if(!Line.some(f => f === null)){
         copyboard.fill(null, x*i, x*(i+1))
@@ -482,7 +471,7 @@ class Tetris extends React.Component {
   }
 
   cleateblock(){
-    const blocks = blocktemplate(midleX, x)
+    const blocks = blocktemplate(middleX, x)
     return blocks[this.getRandomBlock()]
   }
   
@@ -490,34 +479,27 @@ class Tetris extends React.Component {
     const history = this.state.histories;
     const current = history[history.length - 1];
     const board = current.board;
+    const nextBlock = this.state.nextBlock.figures;
 
     if(board.includes("delete")){
       this.delete();
     }
-    const timeId = setTimeout(this.mainloop.bind(this), 1000)
 
-    //ブロックを作成する（For creating blocks）
-    if(this.state.currentBlock.color === null){
-      this.setState({
-        // currentBlock: blocks[this.getRandomBlock()],
-        // nextBlock: blocks[this.getRandomBlock()],
-        currentBlock: this.cleateblock(),
-        nextBlock: this.cleateblock(),
-      })
-    }else if(this.state.currentBlock.color === "next"){
-      const nextBlock = this.state.nextBlock.figures;
-      if(nextBlock.find(f => board[f]!== null)){
-        clearTimeout(timeId)
-        this.setState({
-          message: "Game Over",
-        })
-        return;
-      }
+    const timeId = setTimeout(this.mainloop.bind(this), 1000)
+    // ブロックを作成する（For creating blocks）
+    if(this.state.currentBlock.color === "next"){
       this.setState({
         currentBlock: this.state.nextBlock,
-        // nextBlock: blocks[this.getRandomBlock()],
         nextBlock: this.cleateblock(),
       })
+    }
+    
+    if(nextBlock.find(f => board[f]!== null)){
+      clearTimeout(timeId)
+      this.setState({
+        message: "Game Over",
+      })
+      return;
     }
     this.fallblock();
   }
@@ -525,15 +507,15 @@ class Tetris extends React.Component {
   startGame(){
     this.mainloop()
   }
-
+  
   componentDidMount() {
     this.startGame();
   }
-
+  
   render() {
     const history = this.state.histories;
     const current = history[history.length - 1];
-
+    
     return(
       <div className="wrapper-container">
         <span className="tetris-container">
